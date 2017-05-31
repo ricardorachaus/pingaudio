af5b50a4a9f5419c0a07bed1142746d7950f9d81
ricardorachaus <ricardorachaus@gmail.com>
2017-05-31 17:44:07 -0300

    Classes and Protocols

---
+++ Pingaudio/Pingaudio/PAAudioDataSource.swift
@@ -0,0 +1,13 @@
+//
+//  PAAudioDataSource.swift
+//  Pingaudio
+//
+//  Created by Rachaus on 31/05/17.
+//  Copyright © 2017 Rachaus. All rights reserved.
+//
+
+import AVFoundation
+
+protocol PAAudioDataSource {
+    
+}
---
+++ Pingaudio/Pingaudio/Pingaudio.h
@@ -0,0 +1,19 @@
+//
+//  Pingaudio.h
+//  Pingaudio
+//
+//  Created by Rachaus on 31/05/17.
+//  Copyright © 2017 Rachaus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pingaudio.
+FOUNDATION_EXPORT double PingaudioVersionNumber;
+
+//! Project version string for Pingaudio.
+FOUNDATION_EXPORT const unsigned char PingaudioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pingaudio/PublicHeader.h>
+
+
