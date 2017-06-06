a96f961d6df24b782698c69b8bf7ed0dcdaf33e1
Miguel Nery <nery.mik@gmail.com>
2017-06-06 10:46:05 -0300

    PAAidoManagerDelegate documented

--- Pingaudio/Pingaudio/PAAudioManagerDelegate.swift
+++ Pingaudio/Pingaudio/PAAudioManagerDelegate.swift
@@ -9,5 +9,11 @@
 import AVFoundation
 
 protocol PAAudioManagerDelegate {
+    /**
+     Merge multiple audios in one single audio. The audios are appended, in the order they come in the `audios` array, to the first audio of the array.
+     - Parameter audios: Array containing the audios.
+     - Parameter completion: a block to be executed when merging is complete, wheter it succeeds or fail. The ouptut of the merge is an `URL` pointing to the merged audio in a temporary directory.
+     - Parameter output: Path to the merged file if it succeeds, or `nil` if it fails.
+     */
     func merge(audios: [PAAudio], completion: @escaping (_ output: URL?) -> Void)
 }
