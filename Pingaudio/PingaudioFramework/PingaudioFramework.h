a401ecfafe9523fbdb3eaa56fe982c95dac6cb1c
Miguel Nery <nery.mik@gmail.com>
2017-07-15 11:13:17 -0300

    more sharing scheme

---
+++ Pingaudio/PingaudioFramework/PingaudioFramework.h
@@ -0,0 +1,19 @@
+//
+//  PingaudioFramework.h
+//  PingaudioFramework
+//
+//  Created by Miguel Nery on 15/07/17.
+//  Copyright © 2017 Rachaus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PingaudioFramework.
+FOUNDATION_EXPORT double PingaudioFrameworkVersionNumber;
+
+//! Project version string for PingaudioFramework.
+FOUNDATION_EXPORT const unsigned char PingaudioFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PingaudioFramework/PublicHeader.h>
+
+
